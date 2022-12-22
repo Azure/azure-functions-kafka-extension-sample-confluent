93d2fb457b524994f50a774c1be100e620403ad6
brunolucasblog <bruno.lucas.cloud@outlook.com>
2022-12-22 22:50:38 +1300

    Updated version

--- kafka_example.cs
+++ kafka_example.cs
@@ -1,29 +1,27 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Extensions.Kafka;
-using System.Threading;
+using Microsoft.Extensions.Logging;
 
-namespace kdotnet
+namespace azure_functions_kafka_extension_sample_confluent
 {
-    public static class kafka_example
+    public class kafka_example
     {
-        [FunctionName("kafkaApp")]
-        public static void ConfluentCloudStringTrigger(
-             [KafkaTrigger(
-                "BootstrapServer",
-                "users",
-                ConsumerGroup = "<ConsumerGroup>",
-                Protocol = BrokerProtocol.SaslSsl,
-                AuthenticationMode = BrokerAuthenticationMode.Plain,
-                Username = "<APIKey>",
-                Password = "<APISecret>",
-                SslCaLocation = "confluent_cloud_cacert.pem")]
-        KafkaEventData<string> kafkaEvent,
-        ILogger logger)
-        {	    
-            logger.LogInformation(kafkaEvent.Value.ToString());
+        
+        [FunctionName("kafka_example")]
+        public void Run(
+            [KafkaTrigger("pkc-4j8dq.southeastasia.azure.confluent.cloud:9092",
+                          "topic_0",
+                          Username = "XA7O3AF2JBXUQDVU",
+                          Password = "9tu6Ywa5fKRezcxJ7327uSvr7aLWsIvWAwNFyzSPD+rMx0K5Oe3c8lgdQhA9OezO",
+                          Protocol = BrokerProtocol.SaslSsl,
+                          AuthenticationMode = BrokerAuthenticationMode.Plain,
+                          ConsumerGroup = "$Default")] KafkaEventData<string>[] events,
+            ILogger log)
+        {
+            foreach (KafkaEventData<string> eventData in events)
+            {
+                log.LogInformation($"C# Kafka trigger function processed a message: {eventData.Value}");
+            }
         }
     }
 }
